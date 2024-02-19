271cc920cf47652652e48bc674f3982ea3078a6e
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2024-02-19 16:59:35 -0300

    style: add background message

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -8,7 +8,7 @@ export default function Button({
   [x: string]: any
 }) {
   return (
-    <button className={`bg-blue-500 text-white px-4 py-2 rounded-full  ${className}`} {...props}>
+    <button className={`text-white px-4 py-2 rounded-full  ${className}`} {...props}>
       {children}
     </button>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,23 @@
 import { Roboto } from "next/font/google"
-import Button from "@/components/ui/button"
-import Input from "@/components/ui/input"
 import { BiBook } from "react-icons/bi"
 import { FaLightbulb } from "react-icons/fa"
+import Button from "@/components/ui/button"
+import Input from "@/components/ui/input"
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] })
 
 export default function Home() {
   return (
-    <div className={`flex min-h-screen flex-col items-center justify-between p-24 ${roboto.className}`}>
+    <div className={`flex h-screen flex-col items-center justify-between p-8 sm:p-24 ${roboto.className}`}>
       <header className="flex flex-row justify-center items-center">
         <div className="flex items-center justify-center w-full">
-          <h1 className="text-6xl font-bold">
-            <BiBook className="text-6xl inline-block mr-2" />
+          <h1 className="text-4xl sm:text-6xl font-bold">
+            <BiBook className="text-4xl sm:text-6xl inline-block mr-2" />
             The Bible
           </h1>
         </div>
-        <p className="text-6xl">|</p>
-        <p className="text-sm">Sua IA para ajuda biblica</p>
+        <p className="text-4xl sm:text-6xl">|</p>
+        <p className="text-sm">Sua IA para ajuda bíblica</p>
       </header>
       <main className="flex flex-col items-center justify-center w-full">
         <div className="flex w-full max-w-4xl items-center mb-6">
@@ -28,33 +28,35 @@ export default function Home() {
               placeholder="Faça sua pergunta..."
             />
             <Button
-              className="absolute inset-y-0 right-0 px-4 text-white rounded-full rounded-l-none"
-              style={{ backgroundColor: "rgb(var(--background-start-rgb))", _hover: { opacity: 0.5 } }}
+              className="absolute inset-y-0 right-0 px-4 text-white rounded-full rounded-l-none border border-gray-300"
+              style={{ backgroundColor: "rgb(var(--button-color))", _hover: { opacity: 0.5 } }}
             >
               Perguntar
             </Button>
           </div>
         </div>
-        <div className="flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <div className="flex justify-center border-b border-neutral-800 bg-zinc-800/60 from-inherit static w-auto rounded-xl borderp-4 p-4">
           <div>
             <div className="flex items-center">
-              <FaLightbulb className="text-3xl" />
-              <h2 className="text-2xl font-bold ml-2">Como funciona?</h2>
+              <FaLightbulb className="text-2xl sm:text-3xl" />
+              <h2 className="text-xl sm:text-2xl font-bold ml-2">Como funciona?</h2>
             </div>
-            <p className="ml-2">
-              Faça uma pergunta e receba uma resposta baseada na Bíblia. <br />
+            <div>
+              <p>
+                Faça uma pergunta e receba uma resposta baseada na Bíblia. <br />
+              </p>
               <p>
                 Exemplo:{" "}
                 <b>
                   <q>Estou passando por problemas de ansiedade</q>
                 </b>
               </p>
-            </p>
+            </div>
           </div>
         </div>
       </main>
       <footer className="flex items-center justify-center w-full">
-        <p>© 2024 - Softnation</p>
+        <p>© {new Date().getFullYear()} - Softnation</p>
       </footer>
     </div>
   )
