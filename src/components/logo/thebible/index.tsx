d713c92f5180c4f65c150aa5830c1fba1ade144b
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2024-02-20 00:19:59 -0300

    style: items center logo

--- src/components/logo/thebible/index.tsx
+++ src/components/logo/thebible/index.tsx
@@ -1,26 +1,28 @@
 import { BiBook } from "react-icons/bi"
 
-export default function TheBibleLogo({ color }: { color: "white" | "black" }) {
+interface TheBibleLogoProps {
+  color: "white" | "black"
+}
+
+export default function TheBibleLogo({ color }: TheBibleLogoProps) {
+  const textColor = color === "white" ? "var(--white)" : "rgb(var(--button-color))"
+
   return (
     <div className="flex">
       <div className="flex items-center justify-center w-full">
-        <h1
-          className="text-4xl sm:text-6xl font-bold"
-          style={{ color: color === "white" ? "var(--white)" : "rgb(var(--button-color))" }}
-        >
+        <h1 className="text-4xl sm:text-6xl font-bold" style={{ color: textColor }}>
           <BiBook className="text-4xl sm:text-6xl inline-block mr-2" />
           The Bible
         </h1>
       </div>
-      <p
-        className="text-4xl sm:text-6xl"
-        style={{ color: color === "white" ? "var(--white)" : "rgb(var(--button-color))" }}
-      >
-        |
-      </p>
-      <p className="text-sm" style={{ color: color === "white" ? "var(--white)" : "rgb(var(--button-color))" }}>
-        Sua IA para ajuda bíblica
-      </p>
+      <div className="flex justify-center items-center">
+        <p className="text-4xl sm:text-6xl" style={{ color: textColor }}>
+          |
+        </p>
+        <p className="text-sm" style={{ color: textColor }}>
+          Sua IA para ajuda bíblica
+        </p>
+      </div>
     </div>
   )
 }
