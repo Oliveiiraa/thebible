fbb8b60236c02e360d2c5f56559043d1ab10aa9a
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2024-02-27 10:53:22 -0300

    feat: alter docker file

--- app/sentry.server.config.ts
+++ app/sentry.server.config.ts
@@ -1,17 +1,9 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from "@sentry/nextjs"
 
-const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN
+const SENTRY_DSN = process.env.SENTRY_DSN
 
 Sentry.init({
   dsn: SENTRY_DSN || "https://8454a6f540dc16660b90a74e4a3adc68@us.sentry.io/4506814822547456",
-
-  // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 })
