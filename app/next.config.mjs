09638d41e844dbb05b7f250a54ec99ebcbc71046
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2024-02-27 13:29:31 -0300

    chore: alter next.config

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -9,6 +9,8 @@ export default withSentryConfig(nextConfig, {
   silent: true,
   org: "softnation-i7",
   project: "the-bible",
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  ignore: ["node_modules", "webpack.config.js"],
 }, {
   widenClientFileUpload: true,
   transpileClientSDK: true,
