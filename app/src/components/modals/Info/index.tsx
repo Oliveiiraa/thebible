d9eed3a6036408ce2bf256a5ed9b09605c9ddcae
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2024-02-27 23:43:06 -0300

    styel: alter max-w

--- app/src/components/modals/Info/index.tsx
+++ app/src/components/modals/Info/index.tsx
@@ -46,7 +46,7 @@ const InfoModal = ({ setModalOpen }: InfoModalProps) => {
       {showModal ? (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className={`relative w-auto my-6 mx-auto max-w-${isMobile ? "sm" : "3xl"}`}>
+            <div className={`relative w-auto my-6 mx-auto max-w-3xl`}>
               <div
                 className="border-0 rounded-lg shadow-lg relative flex flex-col w-full outline-none focus:outline-none"
                 style={{ backgroundColor: "var(--white)" }}
